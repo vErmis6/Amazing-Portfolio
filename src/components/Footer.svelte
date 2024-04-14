e20aa3f3d6afa95649c084d646116a1ff9a3ad78
vErmis6 <pyorba6@gmail.com>
2024-04-13 22:28:10 -0700

    first commit

---
+++ src/components/Footer.svelte
@@ -0,0 +1,24 @@
+<footer class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center">
+
+    <p class="px-4 py-2 bg-white text-slate-950 font-medium">
+        Connect with me &darr;
+    </p>
+    <div class="flex flex-coll gap-4 items-center justify-center">
+        <p><b class="pr-2">Email</b> gil.yorba@yahoo.com</p>
+        <p><b class="pr-2">GitHub</b>
+            <a class="text-violet-400" target="_blank" href="https://github.com/vErmis6">
+                vErmis6 <sup><span class="text-xs scale-75 pl-0.5">
+                    <i class="fa-solid fa-arrow-up-right-from-square text-cs scale-[75%]"></i>
+                </span></sup>
+            </a>
+        </p>
+        <p><b class="pr-2">LinkedIn</b>
+            <a class="text-violet-400" target="_blank" href="https://www.linkedin.com/in/gil-yorba-9396212a0/">
+                Gil Yorba <sup><span class="text-xs scale-75 pl-0.5">
+                    <i class="fa-solid fa-arrow-up-right-from-square text-cs scale-[75%]"></i>
+                </span></sup>
+            </a>
+        </p>
+        
+    </div>
+</footer>
---
+++ src/components/Main.svelte
@@ -0,0 +1,149 @@
+<script>
+    import Step from "./Step.svelte";
+
+    let steps = [
+        {name: 'Gym-App', icon:'fa-solid fa-dumbbell'},
+        {name: 'Ultimate Todos', icon:'fa-solid fa-list-check'},
+        {name: 'Pokedex', icon:'fa-solid fa-diagram-project'}
+    ];
+
+    let benefits = [
+        {name: 'A self taught developer', description: "I taught myself to code using free online resources and absolutely fell in love with the creativity and problem solving that is involved in developing and engineering innovative new online experiences. Starting of with JavaScript, HTML & CSS and evolving my knowledge base to include JavaScript frameworks, backend programming, design, cloud services and much much more."},
+        {name: 'A product design & UX finatic', description: "Carefully crafting and designing amazing user experiences allows me to express and experiment with every morsel of creativity I have. I love the challenge of learning new design concepts and enabling users with amazing online experiences."},
+        {name: 'An excellent communicator', description: "Communication is key and it's a paramount value of mine. I believe in transparency and constructive communication above all else. This helps me develop deep relationships and ensures my effectiveness and productivity in any work space with any team."}
+    ];
+
+</script>
+
+<main class="flex flex-col flex-1 p-4">
+    <section id="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
+        <div class="flex flex-col text-center lg:justify-center lg:text-left gap-6 md:gap-8 lg:gap-10">
+            <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+                Hi! I'm <span class="poppins text-violet-400">Gilbert</span> Yorba <br/>Full Stack <span class="poppins text-violet-400">Developer</span>
+            </h2>
+            <p class="text-base sm:text-lg md:text-xl">
+                My <span class="text-violet-400">favorite tech</span> includes JavaScript (NEXT.JS
+                or SvelteKit), TailwindCSS, Node.js + Express.js & PostgreSQL or Firebase/Firestore!
+            </p>
+            <a 
+            href="https://www.linkedin.com/in/gil-yorba-9396212a0/"
+            target="_blank"
+            class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer">
+
+            <div 
+                class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
+            />  
+
+            <h4 class="relative z-9">Get in touch &rarr;</h4>
+
+
+            </a>
+        </div>
+    </section>
+
+
+
+
+    <section id="projects" class="py-80 lg:py-32 flex flex-col gap-24">
+        <div class="flex flex-col gap-2 text-center">
+            <h6 class="text-lg sm:text-text-xl md:text-2xl">
+                A few of my creative endeavors. 
+            </h6>
+            <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+                Curious to <span class="poppins text-violet-400">see</span> my work?
+            </h3>
+           
+        </div>
+        <a href="https://github.com/vErmis6" target="_blank" class="mx-auto px-4 py-2 rounded-md border border-solid border-white flex items-center gap-2 -mb-4 sm:-mb-0 -mt-10 hover:border-violet-700 duration-200">
+            <i class="fa-brands fa-github"></i>
+            <p class="">Check out my GitHub</p>
+        </a>
+        <div class="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10">
+            <Step step={steps[0]}>
+                <p>Gym App is a intricate gym workout app created with <strong class="text-violet-400"> React.js and TailwindCSS.</strong></p>
+            </Step>
+
+            <Step step={steps[1]}>
+                <p>Ultimate Todos is a Full Stack <strong class="text-violet-400">Next.js Node.js + Express.js & Firebase</strong>CRUD application that allows a user to login, manage a tidy and efficacious todo list, and persist this information across devices.</p>
+            </Step>
+
+            <Step step={steps[2]}>
+                <p>The Pokedex is a <strong class="text-violet-400">SvelteKit & TailwindCSS</strong> web application, hosted on <strong class="text-violet-400">Netlify </strong>, that consumes and caches the <strong class="text-violet-400">Pokemon API</strong> to display all Pokemon information.</p>
+            </Step>
+        </div>
+    </section>
+
+    <section id="about" class="py-20 pt-10 lg:pt-16 lg:py-32 flex flex-col gap-16 sm:gap-20 md:gap-24 relative">
+
+        <div class="flex flex-col gap-2 text-center relative before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-700 after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-700 py-4"
+        > 
+        <h6 class="text-lg sm:text-xl md:text-2xl">Want to know more?</h6>
+        <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">A bit <span class="poppins text-violet-400">about</span> me.</h3>
+        </div>
+        <p class="mx-auto poppins font-semibold text-lg sm:text-xl md:text-2xl">I am . . .</p>
+        <div class="flex flex-col gap-20 w-full mx-auto max-w-[800px]">
+            {#each benefits as benefit, index}
+            <div class="flex gap-6 sm:gap-8">
+                <p class="poppins text-4xl sm:text-5xl md:text-6xl text-slate-500 font-semibold">
+                    0{index + 1}
+                </p>
+                <div class="flex flex-col gap-6 sm:gap-8">
+                    <h3 class="text-2xl sm:text-3xl md:text-5xl">
+                        {benefit.name}
+                    </h3>
+                    <p>{benefit.description}</p>
+                </div>
+            </div>
+            {/each}
+        </div>
+        <h5 class="text-2xl sm:3xl font-semibold text-center poppins">The <span class="text-violet-400 poppins">Complete</span> Package</h5>
+        <div class="flex flex-col overflow-x-scroll gap-10 max-2-[800px] mx-auto w-full">
+            <table class="bg-white text-slate-700 rounded text-center">
+                <thead class="border-b border-solid border-slate-200">
+                    <tr class="">
+                        <th/>
+                        <th class="whitespace-nowrap p-2 px-4">Candidate #1</th>
+                        <th class="whitespace-nowrap p-2 px-4">Candidate #2</th>
+                        <th class="whitespace-nowrap p-2 px-4">Candidate #3</th>
+                        <th class="whitespace-nowrap bg-violet-700 text-white p-4 px-8">Me</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="border-b border-solid border-slate-200">
+                        <td class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm">Dedication</td>
+                    <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
+                    <td><i class="fa-solid fa-check text-slate-500"></i></td>
+                    <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
+                    <td><i class="fa-solid fa-check text-green-500"></i></td>
+                    </tr>
+                    <tr class="border-b border-solid border-slate-200">
+                        <td class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm">Critical Thinking</td>
+                    <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
+                    <td><i class="fa-solid fa-check text-slate-500"></i></td>
+                    <td><i class="fa-solid fa-check text-slate-500"></i></td>
+                    <td><i class="fa-solid fa-check text-green-500"></i></td>
+                    </tr>
+                    <tr class="border-b border-solid border-slate-200">
+                        <td class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm">Interpersonal Skills</td>
+                    <td><i class="fa-solid fa-check text-slate-500"></i></td>
+                    <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
+                    <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
+                    <td><i class="fa-solid fa-check text-green-500"></i></td>
+                    </tr>
+                    <tr class="border-b border-solid border-slate-200">
+                        <td class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm">Programming ability</td>
+                    <td><i class="fa-solid fa-check text-slate-500"></i></td>
+                    <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
+                    <td><i class="fa-solid fa-check text-slate-500"></i></td>
+                    <td><i class="fa-solid fa-check text-green-500"></i></td>
+                    </tr>         
+                </tbody>
+            </table>
+        </div>
+        <div class="mx-auto -mt-12 italic sm:hidden opacity-50">
+            <p>Scroll to see more &rarr;</p>
+        </div>
+        <p class="mx-auto">So why not invest?</p>
+    </section>
+
+</main>
